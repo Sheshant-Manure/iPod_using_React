5446b2d16c998f64c077ebbe41d8daeeec838fb5
Sheshant-Manure <msheshant1997@gmail.com>
2023-09-26 20:07:12 +0530

    Feat - click sound on iPod btn press

--- src/Components/Ipodicons.jsx
+++ src/Components/Ipodicons.jsx
@@ -1,16 +1,22 @@
 import { icons } from "../Icondata/Icondata";
 import styles from '../Styles/ipod.module.css';
+import audio from "../Audio/buttonclick.mp3";
 
 export const Ipodicons = () => {
-    return (
-        <div className={styles.buttons}>
-            {
-                icons.map((icon) => {
-                    return (
-                        <img key={icon.id} className={`${styles[icon.alt]}`} src={icon.src} alt={icon.alt}/>
-                    )
-                })
-            }
-        </div>
-    );
+  const playaudio=()=>{
+    console.log("hi");
+    const audio1 = new Audio(audio);
+    audio1.play();
+  }
+  return (
+    <div className={styles.buttons}>
+      {icons.map((icon) => {
+        return (
+          <img key={icon.id} className={`${styles[icon.alt]}`} src={icon.src} alt={icon.alt}  
+            onClick={playaudio}
+          />
+        );
+      })}
+    </div>
+  );
 }
