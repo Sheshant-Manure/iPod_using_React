2f24f02d847a3b6c4a63bb918e63130329ce00d6
Sheshant-Manure <msheshant1997@gmail.com>
2023-09-26 15:11:17 +0530

    Design - Ipod HTML, CSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Importing the Global CSS
+import "./Styles/style.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
